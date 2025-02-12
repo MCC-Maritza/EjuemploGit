23ea629f753c534e41456b10e0bbb44d483ebbe2
MCC-Maritza <m.c.c.maritza@outlook.es>
2025-02-11 18:49:50 -0600

    Se sube programa

---
+++ src/ejuemplogit/EjuemploGit.java
@@ -0,0 +1,15 @@
+
+package ejuemplogit;
+
+/**
+ *
+ * @author maryse
+ */
+public class EjuemploGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
