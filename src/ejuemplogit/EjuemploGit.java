15d3954527981362e6a82da0655eb1d7f31a6ab2
MCC-Maritza <m.c.c.maritza@outlook.es>
2025-02-13 17:59:27 -0600

    Se implementa entrada de datos con Scanner

--- src/ejuemplogit/EjuemploGit.java
+++ src/ejuemplogit/EjuemploGit.java
@@ -1,15 +1,32 @@
 
 package ejuemplogit;
+import java.util.Scanner;
 
 /**
  *
  * @author maryse
  */
+
 public class EjuemploGit {
+    
+    Scanner entrada = new Scanner (System.in);    
+    String mensaje;
+    
+    public  void Mensaje(){
+        
+        mensaje= entrada.nextLine();
+        System.out.println("El mensaje es: " + mensaje);
+        
+        
+    }
+    
 
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        //System.out.println("Hola Mundo");
+        
+        EjuemploGit ejemplo= new EjuemploGit();
+        ejemplo.Mensaje();
     }
     
 }
